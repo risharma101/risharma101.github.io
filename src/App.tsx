b32105164670e3ddbbdb4ad603d1e18a4af740b0
Rishabh Sharma <rishabhsharma7676@gmail.com>
2024-02-14 00:13:55 -0800

    updated router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import HomePage from './pages/HomePage';
 import ProjectPage from './pages/ProjectPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <div className="nav-bar">
         <NavBar />
       </div>
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/projects" element={<ProjectPage />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
