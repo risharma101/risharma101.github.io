7235368b4c77c1e51a74f009ec9e35d93b406734
Rishabh Sharma <rishabhsharma7676@gmail.com>
2024-02-14 09:30:52 -0800

    updated url

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,14 +9,19 @@ const HomePage = () => {
                 <h1>Rishabh Sharma</h1>
 
             </div>
-            <div className='about-me' style={{ display: 'flex', alignItems: 'center', margin: '20px 0' }}>
-                <div className='about-section' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '20px 0' }}>
-                    <a href='https://github.com/risharma101'>Github</a>
+            <div className='about-section'>
+                <div className='about-column'>
+                    {/* <a href='https://github.com/risharma101'>Github</a>
                     <a href='https://www.linkedin.com/in/rishabhsharma101/'>LinkedIn</a>
-                    <a>rishabhsharma@ucla.edu</a>
+                    <a>rishabhsharma@ucla.edu</a> */}
+                    <p>THIS IS PURELY A TEST TO SEE</p>
                 </div>
-                <div className='about-section'><img src={profilepic} alt="Your Name" style={{ width: '200px', borderRadius: '5%', marginRight: '20px' }} /></div>
-                <div className='about-section'><p>I'm Rishabh!. Feel free to expand this section with more information about yourself.</p></div>
+                <div className='about-img'>
+                    <img src={profilepic} alt="Your Name" />
+                </div>
+                <div className='about-column'>
+                    <p>I'm Rishabh!. Feel free to expand this section with more information about yourself.</p>
+                </div>
             </div>
             <footer style={{ marginTop: 'auto', padding: '20px 0', color: 'white', width: '100%', textAlign: 'center' }}>
                 Designed and built by Rishabh Sharma
